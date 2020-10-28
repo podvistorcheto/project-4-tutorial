2bad8faf798f8522c15cec4ad416de6125ba437c
podvistorcheto <denev.s.deyan@gmail.com>
2020-10-28 11:02:30 +0000

    09-Added views methods for the permissions.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles import serializers
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
+
 from profiles import models
+from profiles import permissions
+from profiles import serializers
+
 
 
 class HelloApiView(APIView):
@@ -99,3 +103,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """manages creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
