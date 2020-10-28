e28390ffee9f4ba46ea10fc3357765c017ba7ba0
podvistorcheto <denev.s.deyan@gmail.com>
2020-10-28 09:14:16 +0000

    06-Added viewset to the main app

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from profiles import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 
 urlpatterns = [
-    path('home', views.HelloApiView.as_view()),
+    path('home/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
+    #path('', views.HelloViewSet.as_view({'get': 'list'})),
 ]
