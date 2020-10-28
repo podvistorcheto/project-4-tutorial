b48e15d7fb746c8794d846153bfd588fb2647fcc
podvistorcheto <denev.s.deyan@gmail.com>
2020-10-28 14:08:07 +0000

    12-Added methods for handling CRUD allow users to update only their own status

--- profiles/views.py
+++ profiles/views.py
@@ -6,11 +6,11 @@ from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from profiles import models
 from profiles import permissions
 from profiles import serializers
-
 
 
 class HelloApiView(APIView):
@@ -122,6 +122,10 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly
+    )
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
